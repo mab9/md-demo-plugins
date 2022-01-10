a24fbbbfcd91df857d0a33b9b44a95d6b86668d9
marc-antoine <marcantoine.bruelhart@gmail.com>
2022-01-10 13:53:57 +0100

    Add new goto resources

--- gotos/resource.sh
+++ gotos/resource.sh
@@ -4,9 +4,13 @@ mdConfigFile=${HOME}/.md
 sourceDir=$(cat $mdConfigFile | grep -i source-dir | cut -d "=" -f 2)
 
 alias goto-md-src="cd ${MD_INSTALLATION_FOLDER}"
+alias goto-cs-src="cd ${sourceDir}/cheatsheets"
+
+alias goto-vakansie-src="cd ~/development/source/vakansie"
+alias goto-plansch-src="cd ~/development/source/fhnw/plansch"
+alias goto-calendario-src="cd ${sourceDir}/calendario"
+
 alias goto-svv-src="cd ${sourceDir}/svvstatistikserver"
 alias goto-svv-src-dep="cd ${sourceDir}/svvstatistikserver/statistikserver-deployment/docker/helm/statistikserver"
 alias goto-dab-src="cd ${sourceDir}/dabbawala"
-alias goto-cs-src="cd ${sourceDir}/cheatsheets"
-alias goto-vakansie-src="cd ~/development/source/vakansie"
-alias goto-plansch-src="cd ~/development/source/fhnw/plansch"
+alias goto-gef-src="cd ${sourceDir}/gef/gef-dabbawala"
